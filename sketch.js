cb40a47ed9ca4e14550e727158859339962a45b9
akash787 <akashshelke7878@gmail.com>
2021-04-15 15:26:16 +0530

    trial class

--- sketch.js
+++ sketch.js
@@ -23,6 +23,8 @@ function setup(){
     box2 = new Box(920,320,70,70);
     pig1 = new Pig(810, 350);
     log1 = new Log(810,260,300, PI/2);
+    newlog = new Log(50,50,50, PI/2);
+
 
     box3 = new Box(700,240,70,70);
     box4 = new Box(920,240,70,70);
@@ -61,4 +63,5 @@ function draw(){
 
     bird.display();
     platform.display();
+    newlog.display();
 }
